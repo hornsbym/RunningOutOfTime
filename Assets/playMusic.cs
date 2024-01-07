6dee4d2ea298a5114d577bc3a879543893b6a3ac
Huffmanc33 <huffmancarterd@gmail.com>
2024-01-07 12:21:12 -0500

    Added music

--- Assets/playMusic.cs
+++ Assets/playMusic.cs
@@ -1,23 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class playMusic : MonoBehaviour
 {
-    
-    private float playSong1 = 30;
-    private float playSong2 = 60;
-    private float playSong3 = 90;
-    private float playSong4 = 120;
-    private float playSong5 = 150;
+    private float playSong = 120;
     private float timePassed = 0;
-    private bool inMenu = true;
-    private AudioSource myAudioSource;
+    private float loops = 0;
+    private bool inMenu = false;
+    public AudioSource Menu;
+    public AudioSource Future;
+    public AudioSource Samurai;
+    public AudioSource BigCarTheft;
     // Start is called before the first frame update
     void Start()
     {
-        myAudioSource = GetComponent<AudioSource>();
+ 
     }
 
     // Update is called once per frame
@@ -25,22 +25,42 @@ public class playMusic : MonoBehaviour
     {
         if(inMenu == false){
             timePassed += Time.deltaTime;
-            if (timePassed == playSong1)
+            if (timePassed >= playSong)
             {
-                // myAudioSource.clip()
-                myAudioSource.Play();
-            }
-            else if (timePassed == playSong2)
-            {
-                myAudioSource.Play();
-            }
-            else if (timePassed == playSong3)
-            {
-                myAudioSource.Play();
-            }
-            else if (timePassed == playSong4)
-            {
-                myAudioSource.Play();
+                timePassed = 0;
+                loops += 1;
+                switch(loops){
+                    case 1:
+                        StartCoroutine(AudioFadeScript.FadeOut(Menu, 0.8f));
+                        StartCoroutine(AudioFadeScript.FadeIn(Future, 8f));
+                        // Menu.Stop();
+                        // Future.Play();
+                        break;
+                    case 2:
+                        StartCoroutine(AudioFadeScript.FadeOut(Future, 0.5f));
+                        StartCoroutine(AudioFadeScript.FadeIn(Samurai, 5f));
+                        // Future.Stop();
+                        // Samurai.Play();
+                        break;
+                    case 3:
+                        StartCoroutine(AudioFadeScript.FadeOut(Samurai, 0.8f));
+                        StartCoroutine(AudioFadeScript.FadeIn(BigCarTheft, 8f));
+                        // Samurai.Stop();
+                        // BigCarTheft.Play();
+                        break;
+                    case 4:
+                        StartCoroutine(AudioFadeScript.FadeOut(BigCarTheft, 0.8f));
+                        StartCoroutine(AudioFadeScript.FadeIn(Menu, 8f));
+                        // BigCarTheft.Stop();
+                        // Menu.Play();
+                        break;
+                    case 5:
+                        StartCoroutine(AudioFadeScript.FadeOut(Menu, 0.8f));
+                        StartCoroutine(AudioFadeScript.FadeIn(BigCarTheft, 8f));
+                        // Menu.Stop();
+                        // Samurai.Play();
+                        break;
+                }
             }
         }
     }
